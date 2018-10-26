712b02d21a1e18789a30b3b565f0c4fd5f61f1bc
Luke Stevens <lcm.stevens96@gmail.com>
2018-10-26 15:47:46 +0100

    Show game setup component on home screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameSetupComponent } from './game-setup/game-setup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameSetupComponent
   ],
   imports: [
     BrowserModule
