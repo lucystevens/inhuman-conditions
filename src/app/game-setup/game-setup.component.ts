750e1f0296af9c7696d699a4f65859aef19353d5
lukecmstevens <lcm.stevens96@gmail.com>
2018-10-31 19:30:02 +0000

    Add final step with link to send to suspect

--- src/app/game-setup/game-setup.component.ts
+++ src/app/game-setup/game-setup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GameSetup } from '../card-definitions';
+import { GameService } from '../services/game.service';
 
 @Component({
   selector: 'app-game-setup',
@@ -9,12 +10,27 @@ import { GameSetup } from '../card-definitions';
 export class GameSetupComponent implements OnInit {
 
   setup: GameSetup;
+  private url: string;
 
-  constructor() { }
+  constructor(private setupService: GameService) { }
 
   ngOnInit() {
     this.setup = new GameSetup();
     this.setup.step = 1;
+  }
+
+  getURL() : string {
+    if(!this.url){
+      const baseUrl = "https://lukecmstevens/inhuman-conditions/suspect/";
+      this.url = baseUrl + this.setupService.encodeGame(this.setup);
+    }
+    return this.url;
+  }
+
+  copy(inputElement){
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.setSelectionRange(0, 0);
   }
 
 }
